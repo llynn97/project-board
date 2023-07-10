d8a6ea93c94e38c914d92d0d590f89e9480ec387
이다은 <llynn97@naver.com>
2023-07-11 04:25:34 +0900

    #14 - AuditingFields 추출
    엔티티 클래스에 들어가는 요소이면서 도메인과 직접 연관이 없는 요소이므로 추출
    `@MappedSuperClass` 이용하여 상속 방식으로 추출 
    
    `@DateTimeFormat` 요소 추가

--- project-board/src/main/java/com/project/projectboard/domain/Article.java
+++ project-board/src/main/java/com/project/projectboard/domain/Article.java
@@ -22,9 +22,9 @@ import java.util.Set;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy"),
 })
-@EntityListeners(AuditingEntityListener.class)
 @Entity
-public class Article {
+public class Article extends AuditingFields {
+
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
 
     @Setter @Column(nullable = false) private String title;
@@ -35,11 +35,6 @@ public class Article {
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     @ToString.Exclude  //순환참조 막기 위해서
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
-
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;
-    @CreatedBy @Column(nullable = false, length = 100) private String createdBy;
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;
-    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;
 
     protected Article() {
     }
