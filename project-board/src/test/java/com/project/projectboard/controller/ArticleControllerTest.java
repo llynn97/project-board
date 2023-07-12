93769bdb639c0896866acb2440ba0590960d4373
이다은 <llynn97@naver.com>
2023-07-12 20:45:43 +0900

    #18 - 컨트롤러 테스트 업데이트

--- project-board/src/test/java/com/project/projectboard/controller/ArticleControllerTest.java
+++ project-board/src/test/java/com/project/projectboard/controller/ArticleControllerTest.java
@@ -42,7 +42,7 @@ class ArticleControllerTest {
     void givenAuthorizedUser_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -55,7 +55,7 @@ class ArticleControllerTest {
 
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search"));
     }
 
@@ -65,7 +65,7 @@ class ArticleControllerTest {
     void givenHashtag_whenRequestingArticleSearchHashtagView_thenReturnsArticleSearchHashtagView() throws Exception {
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search-hashtag"));
 
     }
