367eee64253996d3488febf602b73248209f540b
이다은 <llynn97@naver.com>
2023-07-12 19:10:57 +0900

    #18 - 게시글 관련 뷰 테스트 작성

---
+++ project-board/src/main/java/com/project/projectboard/controller/ArticleController.java
@@ -0,0 +1,15 @@
+package com.project.projectboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 	/articles
+ *  /articles/{article-id}
+ * 	/articles/search
+ * 	/articles/search-hashing
+ **/
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}
