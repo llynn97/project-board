ee70c7dcd64092e32d9a4001d4dd4d32e6667058
이다은 <llynn97@naver.com>
2023-07-13 17:09:58 +0900

    #18 -  게시글 뷰 기본 골격 작성

--- project-board/src/test/java/com/project/projectboard/controller/ArticleControllerTest.java
+++ project-board/src/test/java/com/project/projectboard/controller/ArticleControllerTest.java
@@ -36,7 +36,7 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"));
     }
 
-    @Disabled("구현 중")
+
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     void givenAuthorizedUser_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
